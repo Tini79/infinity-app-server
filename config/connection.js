01a316204c10baee4335786836f9cf1c5b2d2a3d
Tini79 <artinichan79@gmail.com>
2024-04-24 20:45:56 +0800

    updated database connection for production

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,13 @@
 const mysql = require("mysql")
 
-const con = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "cakratendados",
-  database: "infinity_db"
-})
+const urlDB = `mysql://root:HzwwQzvnzNaWHSeHcLjrnRsuiNSXnSQL@roundhouse.proxy.rlwy.net:44797/railway`
+const con = mysql.createConnection(urlDB)
+// const con = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "cakratendados",
+//   database: "infinity_db"
+// })
 
 con.connect((err) => {
   if (err) throw err
