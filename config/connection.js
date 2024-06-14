e55905779afd46fdef0011552f008e4d884f285d
Tini79 <artinichan79@gmail.com>
2024-06-14 23:20:11 +0800

    set connection to local

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,14 @@
 require("dotenv").config()
 const mysql2 = require("mysql2")
 
-const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASS}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDB}`
-const con = mysql2.createConnection(urlDB)
-// const con = mysql2.createConnection({
-//   host: "localhost",
-//   user: "root",
-//   password: "cakratendados",
-//   database: "infinity_db"
-// })
+// const urlDB = `mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASS}@${process.env.MYSQLHOST}:${process.env.MYSQLPORT}/${process.env.MYSQLDB}`
+// const con = mysql2.createConnection(urlDB)
+const con = mysql2.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "cakratendados",
+  database: "infinity_db"
+})
 
 con.connect((err) => {
   if (err) {
